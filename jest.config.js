7f6c9c23dd007ac1e75287d9b0c70dd6e9f91e65
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-04-18 17:23:53 +0100

    ci: add moduleNameMappers to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,14 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
+
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/src'
+  }),
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
