fbc73d624e76d99666684ed26c47f9fffec395c1
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-06-02 08:13:08 +0100

    refactor: move logo to a component

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 
 import Spinner from '@presentation/components/spinner';
+import Logo from '@presentation/components/logo';
 import Styles from './styles.scss';
 
 const Login: React.FC = () => (
   <div className={Styles.login}>
     <header className={Styles.header}>
-      <img src="/assets/logo.svg" alt="4Dev Logo" height={80} width={80} />
+      <Logo fill="white" height={80} width={80} />
       <h1>4Dev - Surveys for Developers</h1>
     </header>
     <form className={Styles.form} action="">
