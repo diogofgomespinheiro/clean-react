cbc5230f21b62b058c09f65dff72cb2ce8d24f43
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-07-04 00:44:55 +0100

    test: ensure MinLengthValidator returns error if value is invalid

---
+++ src/validation/validators/min-length/min-length-validator.spec.ts
@@ -0,0 +1,33 @@
+import faker from 'faker';
+
+import { InvalidFieldError } from '@validation/errors';
+import { MinLengthValidator } from './min-length-validator';
+
+type SutTypes = {
+  fieldName: string;
+  minLength: number;
+  sut: MinLengthValidator;
+};
+
+const makeSut = (
+  fieldName = faker.random.word(),
+  minLength = faker.datatype.number(10)
+): SutTypes => ({
+  fieldName,
+  minLength,
+  sut: new MinLengthValidator(fieldName, minLength)
+});
+
+describe('MinLengthValidato', () => {
+  it('should return error if value is invalid', () => {
+    const { fieldName, minLength, sut } = makeSut();
+
+    const error = sut.validate(faker.datatype.string(minLength - 1));
+    expect(error).toEqual(
+      new InvalidFieldError(
+        fieldName,
+        `This field must have at least ${minLength} characters`
+      )
+    );
+  });
+});
