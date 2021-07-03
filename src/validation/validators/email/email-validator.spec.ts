b51b19675ab2951fde586211c6481a9acf15417b
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-07-03 22:36:31 +0100

    test: ensure EmailValidator returns false if email is valid

--- src/validation/validators/email/email-validator.spec.ts
+++ src/validation/validators/email/email-validator.spec.ts
@@ -1,11 +1,24 @@
+import faker from 'faker';
+
 import { InvalidFieldError } from '@validation/errors';
 import { EmailValidator } from './email-validator';
 
+const makeSut = (field = faker.random.word()): EmailValidator =>
+  new EmailValidator(field);
+
 describe('EmailValidator', () => {
   it('should return error if email is invalid', () => {
-    const sut = new EmailValidator('email');
+    const field = faker.random.word();
+    const sut = makeSut(field);
 
-    const error = sut.validate('');
-    expect(error).toEqual(new InvalidFieldError('email'));
+    const error = sut.validate(faker.random.word());
+    expect(error).toEqual(new InvalidFieldError(field));
+  });
+
+  it('should return falsy if email is valid', () => {
+    const sut = makeSut();
+
+    const error = sut.validate(faker.internet.email());
+    expect(error).toBeFalsy();
   });
 });
