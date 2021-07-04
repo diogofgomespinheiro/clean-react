db0b04f9f5bec06b4c631f77ecb17ec3da92eefd
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-07-04 01:52:12 +0100

    test: ensure ValidatorBuilder has a min helper method

--- src/validation/validators/builder/validator-builder.spec.ts
+++ src/validation/validators/builder/validator-builder.spec.ts
@@ -1,4 +1,8 @@
-import { RequiredFieldValidator, EmailValidator } from '@validation/validators';
+import {
+  RequiredFieldValidator,
+  EmailValidator,
+  MinLengthValidator
+} from '@validation/validators';
 import { ValidatorBuilder as sut } from './validator-builder';
 
 describe('ValidatorBuilder', () => {
@@ -10,5 +14,13 @@ describe('ValidatorBuilder', () => {
   it('should return EmailValidator', () => {
     const validators = sut.field('any_field').email().build();
     expect(validators).toEqual([new EmailValidator('any_field')]);
+  });
+
+  it('should return MinLengthValidator', () => {
+    const minLength = 5;
+    const validators = sut.field('any_field').min(minLength).build();
+    expect(validators).toEqual([
+      new MinLengthValidator('any_field', minLength)
+    ]);
   });
 });
