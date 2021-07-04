69ed6d5471d33e8735336c53b18ee2c6de46ae91
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-07-04 01:16:51 +0100

    test: ensure validatorComposite returns the first error found

--- src/validation/validators/validation-composite/validator-composite.spec.ts
+++ src/validation/validators/validation-composite/validator-composite.spec.ts
@@ -4,11 +4,12 @@ import { ValidatorComposite } from './validator-composite';
 describe('ValidatorComposite', () => {
   it('should return error if any validation fails', () => {
     const fieldValidatorSpy = new FieldValidatorSpy('any_field');
+    fieldValidatorSpy.error = new Error('first_error_message');
     const fieldValidatorSpy2 = new FieldValidatorSpy('any_field');
-    fieldValidatorSpy2.error = new Error('any_error_message');
+    fieldValidatorSpy2.error = new Error('second_error_message');
 
     const sut = new ValidatorComposite([fieldValidatorSpy, fieldValidatorSpy2]);
     const error = sut.validate('any_field', 'any_value');
-    expect(error).toBe(fieldValidatorSpy2.error.message);
+    expect(error).toBe(fieldValidatorSpy.error.message);
   });
 });
