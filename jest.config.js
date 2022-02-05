c1c0ad837f8da3b5a5b5899a79cc722a11c6cabc
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2022-02-05 18:04:20 +0000

    ♻️  chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	roots: ['<rootDir>/src'],
+	collectCoverageFrom: [
+		'<rootDir>/src/**/*.{ts,tsx}',
+		'!<rootDir>/src/main/**/*',
+		'!<rootDir>/src/**/index.ts',
+		'!**/*.d.ts',
+	],
+	coverageDirectory: 'coverage',
+	testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/tests/e2e/cypress'],
+	testEnvironment: 'jsdom',
+	transform: {
+		'.+\\.(ts|tsx)$': 'ts-jest',
+	},
+	moduleNameMapper: {
+		'\\.(scss|css|sass)$': 'identity-obj-proxy',
+	},
+};
---
+++ src/a.spec.ts
@@ -0,0 +1,5 @@
+describe('aa', () => {
+	it('should test', () => {
+		expect(1).toBe(1);
+	});
+});
